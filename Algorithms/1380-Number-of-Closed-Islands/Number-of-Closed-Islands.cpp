702f4344cf0ee5e6e28e31b8a72626922974abdb
Mohit Vivek Jain <mohitj8732@gmail.com>
2026-07-07 16:29:10 +0530

    Time: 0 ms (100%) | Memory: 13.1 MB (71%) - LeetCommit

--- Algorithms/1380-Number-of-Closed-Islands/Number-of-Closed-Islands.cpp
+++ Algorithms/1380-Number-of-Closed-Islands/Number-of-Closed-Islands.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+    int m, n;
+    int row[4] = {0, 0, 1, -1}, col[4] = {1, -1, 0, 0};
     void dfs(vector<vector<int>>& grid, int x, int y) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> row = {0, 0, 1, -1}, col = {1, -1, 0, 0};
         grid[x][y] = 1;
         for(int i = 0; i < 4; i++) {
             int X = x + row[i], Y = y + col[i];
@@ -13,7 +13,7 @@ public:
     }
 
     int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        m = grid.size(); n = grid[0].size();
         for(int i = 0; i < m; i++) {
             if(grid[i][0] == 0) dfs(grid, i, 0);
             if(grid[i][n - 1] == 0) dfs(grid, i, n - 1);
